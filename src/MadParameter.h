ce9f09002d251df7d1b40901630c96bcf411235f
jonasfehr <mail@jonasfehr.ch>
2018-07-02 11:41:24 +0200

    fixed

--- src/MadParameter.h
+++ src/MadParameter.h
@@ -15,65 +15,55 @@
 class MadParameter : public ofParameter<float>{
 public:
 	MadParameter(ofJson parameterValues){		
-		// Set values
-		this->setOscAddress(parameterValues
-				 ["FULL_PATH"].get<std::string>());
-		this->setName(parameterValues
-				 ["DESCRIPTION"]);
-		if(!parameterValues
-				 ["RANGE"].is_null() ){
-			this->setMin(parameterValues
-				 ["RANGE"].at(0)["MIN"]);
-			this->setMax(parameterValues
-				 ["RANGE"].at(0)["MAX"]);
-		}
-		this->set(parameterValues
-				 ["VALUE"].at(0));
-	}
+		this->setOscAddress(parameterValues["FULL_PATH"].get<std::string>());
+		this->setName(parameterValues["DESCRIPTION"]);
+        if(!parameterValues["RANGE"].is_null() ){
+            range.min = parameterValues["RANGE"].at(0)["MIN"].get<float>();
+            range.max = parameterValues["RANGE"].at(0)["MAX"].get<float>();
+            //            this->setMin(parameterValues["RANGE"].at(0)["MIN"]);
+            //            this->setMax(parameterValues["RANGE"].at(0)["MAX"]);
+        }
+        float valueNormalized = ofMap(parameterValues["VALUE"].at(0), range.min, range.max, 0, 1);
+        this->set(valueNormalized);
+    };
     
     MadParameter(ofJson parameterValues, string name){
-        this->setOscAddress(parameterValues
-                            ["FULL_PATH"].get<std::string>());
+        this->setOscAddress(parameterValues["FULL_PATH"].get<std::string>());
         this->setName(name);
-        if(!parameterValues
-           ["RANGE"].is_null() ){
-            this->setMin(parameterValues
-                         ["RANGE"].at(0)["MIN"]);
-            this->setMax(parameterValues
-                         ["RANGE"].at(0)["MAX"]);
+        if(!parameterValues["RANGE"].is_null() ){
+            range.min = parameterValues["RANGE"].at(0)["MIN"].get<float>();
+            range.max = parameterValues["RANGE"].at(0)["MAX"].get<float>();
+            //            this->setMin(parameterValues["RANGE"].at(0)["MIN"]);
+            //            this->setMax(parameterValues["RANGE"].at(0)["MAX"]);
         }
-        this->set(parameterValues
-                  ["VALUE"].at(0));
-                     
+        float valueNormalized = ofMap(parameterValues["VALUE"].at(0), range.min, range.max, 0, 1);
+        this->set(valueNormalized);
     };
 
-	
 	MadParameter(){};
+    ~MadParameter(){};
 	
-	
-	~MadParameter(){
-	}
-	
-	float getParameterValue(){
-		return this->get();
-	}
+    float getParameterValue(){
+        float value = ofMap(this->get(), 0, 1, range.min, range.max);
+        return value;
+    }
     
      ofEvent<ofxOscMessage> oscSendEvent;
 
-	
-	
 	string oscAddress;
 	void setOscAddress(string address){ oscAddress = address;}
-	
+    
+    struct Range{
+        float min;
+        float max;
+    } range;
+    	
 	// Send OSC when parameter changed
 	void onParameterChange(float & p){
 		this-set(p);
-		cout << "New param at: " + ofToString(this->get()) << endl;
-        
         ofxOscMessage m;
         m.setAddress(oscAddress);
-        m.addFloatArg(get());
-        
+        m.addFloatArg(getParameterValue());
         ofNotifyEvent(oscSendEvent,m,this);
 	}
 	
